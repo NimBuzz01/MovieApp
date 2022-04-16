97fe7eca81d4c99d46c1a3095708fe5dfc943000
NimBuzz01 <mniamatmarjan@gmail.com>
2022-04-16 16:43:58 +0530

    update #1.4

--- app/src/main/java/com/example/movieapp/AddMovies.kt
+++ app/src/main/java/com/example/movieapp/AddMovies.kt
@@ -2,10 +2,35 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.room.Room
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class AddMovies : AppCompatActivity() {
+
+    private lateinit var table: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_movies)
+
+        table = findViewById(R.id.table)
+
+        val db = Room.databaseBuilder(
+            this, MoviesDatabase::class.java,
+            "myDatabase"
+        ).build()
+        val moviesDao = db.moviesDao()
+
+        runBlocking {
+            launch {
+                val movies: List<Movies> = moviesDao.getAll()
+                for (m in movies) {
+                    table.append("\n ${m.title} , ${m.year}")
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,21 +1,29 @@
 package com.example.movieapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.*
 import androidx.room.Room
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var tv: TextView
+    // api key 7086918a
+    // http://www.omdbapi.com/?apikey=[7086918a]&
+
+    private lateinit var addMoviesBtn: Button
+    private lateinit var searchActorsBtn: Button
+    private lateinit var searchMoviesBtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tv = findViewById(R.id.mainTitle)
+        addMoviesBtn = findViewById(R.id.addMoviesBtn)
+        searchActorsBtn = findViewById(R.id.searchActorsBtn)
+        searchMoviesBtn = findViewById(R.id.searchMoviesBtn)
 
         val db = Room.databaseBuilder(
             this, MoviesDatabase::class.java,
@@ -23,74 +31,83 @@ class MainActivity : AppCompatActivity() {
         ).build()
         val moviesDao = db.moviesDao()
 
-        runBlocking {
-            launch {
-                val movie1 = Movies(
-                    1, "The Shawshank Redemption",
-                    "1994",
-                    "R",
-                    "14 Oct 1994",
-                    "142 min",
-                    "Drama",
-                    "Frank Darabont",
-                    "Stephen King, Frank Darabont",
-                    "Tim Robbins, Morgan Freeman, Bob Gunton",
-                    "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
-                )
-                val movie2 = Movies(
-                    2, "Batman: The Dark Knight Returns, Part 1",
-                    "2012",
-                    "PG-13",
-                    "25 Sep 2012",
-                    "76 min",
-                    "Animation, Action, Crime, Drama, Thriller",
-                    "Jay Oliva",
-                    "Bob Kane (character created by: Batman), Frank Miller (comic book), Klaus Janson (comic book), Bob Goodman",
-                    "Peter Weller, Ariel Winter, David Selby, Wade Williams",
-                    "Batman has not been seen for ten years. A new breed of criminal ravages Gotham City, forcing 55-year-old Bruce Wayne back into the cape and cowl. But, does he still have what it takes to fight crime in a new era?"
-                )
-                val movie3 = Movies(
-                    3, "The Lord of the Rings: The Return of the King",
-                    "2003",
-                    "PG-13",
-                    "17 Dec 2003",
-                    "201 min",
-                    "Action, Adventure, Drama",
-                    "Peter Jackson",
-                    "J.R.R. Tolkien, Fran Walsh, Philippa Boyens",
-                    "Elijah Wood, Viggo Mortensen, Ian McKellen",
-                    "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."
-                )
-                val movie4 = Movies(
-                    3, "Inception",
-                    "2010",
-                    "PG-13",
-                    "16 Jul 2010",
-                    "148 min",
-                    "Action, Adventure, Sci-Fi",
-                    "Christopher Nolan",
-                    "Christopher Nolan",
-                    "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page",
-                    "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."
-                )
-                val movie5 = Movies(
-                    3, "The Matrix",
-                    "1999",
-                    "R",
-                    "31 Mar 1999",
-                    "136 min",
-                    "Action, Sci-Fi",
-                    "Lana Wachowski, Lilly Wachowski",
-                    "Lilly Wachowski, Lana Wachowski",
-                    "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
-                    "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence."
-                )
-                //moviesDao.insertMovies(movie1, movie2, movie3, movie4, movie5)
-                val movies: List<Movies> = moviesDao.getAll()
-                for (m in movies) {
-                    tv.append("\n ${m.title} ${m.actors}")
+        addMoviesBtn.setOnClickListener{
+            runBlocking {
+                launch {
+                    val movie1 = Movies(
+                        1, "The Shawshank Redemption",
+                        "1994",
+                        "R",
+                        "14 Oct 1994",
+                        "142 min",
+                        "Drama",
+                        "Frank Darabont",
+                        "Stephen King, Frank Darabont",
+                        "Tim Robbins, Morgan Freeman, Bob Gunton",
+                        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency."
+                    )
+                    val movie2 = Movies(
+                        2, "Batman: The Dark Knight Returns, Part 1",
+                        "2012",
+                        "PG-13",
+                        "25 Sep 2012",
+                        "76 min",
+                        "Animation, Action, Crime, Drama, Thriller",
+                        "Jay Oliva",
+                        "Bob Kane (character created by: Batman), Frank Miller (comic book), Klaus Janson (comic book), Bob Goodman",
+                        "Peter Weller, Ariel Winter, David Selby, Wade Williams",
+                        "Batman has not been seen for ten years. A new breed of criminal ravages Gotham City, forcing 55-year-old Bruce Wayne back into the cape and cowl. But, does he still have what it takes to fight crime in a new era?"
+                    )
+                    val movie3 = Movies(
+                        3, "The Lord of the Rings: The Return of the King",
+                        "2003",
+                        "PG-13",
+                        "17 Dec 2003",
+                        "201 min",
+                        "Action, Adventure, Drama",
+                        "Peter Jackson",
+                        "J.R.R. Tolkien, Fran Walsh, Philippa Boyens",
+                        "Elijah Wood, Viggo Mortensen, Ian McKellen",
+                        "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring."
+                    )
+                    val movie4 = Movies(
+                        4, "Inception",
+                        "2010",
+                        "PG-13",
+                        "16 Jul 2010",
+                        "148 min",
+                        "Action, Adventure, Sci-Fi",
+                        "Christopher Nolan",
+                        "Christopher Nolan",
+                        "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page",
+                        "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster."
+                    )
+                    val movie5 = Movies(
+                        5, "The Matrix",
+                        "1999",
+                        "R",
+                        "31 Mar 1999",
+                        "136 min",
+                        "Action, Sci-Fi",
+                        "Lana Wachowski, Lilly Wachowski",
+                        "Lilly Wachowski, Lana Wachowski",
+                        "Keanu Reeves, Laurence Fishburne, Carrie-Anne Moss",
+                        "When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence."
+                    )
+                    moviesDao.insertMovies(movie1, movie2, movie3, movie4, movie5)
+                    //val movies: List<Movies> = moviesDao.getAll()
                 }
             }
+            val intent = Intent(this, AddMovies::class.java)
+            startActivity(intent)
+        }
+        searchMoviesBtn.setOnClickListener{
+            val intent = Intent(this, SearchMovies::class.java)
+            startActivity(intent)
+        }
+        searchActorsBtn.setOnClickListener{
+            val intent = Intent(this, SearchActors::class.java)
+            startActivity(intent)
         }
 
     }
